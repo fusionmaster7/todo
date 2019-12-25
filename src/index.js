74b4ed73f2047c0e5bb824ae199214216703d2db
Hardik Singh <hardiksingh297@gmail.com>
2019-12-26 01:22:45 +0530

    Can now add tasks to the todo list.

--- src/index.js
+++ src/index.js
@@ -19,13 +19,28 @@ class App extends React.Component{
   constructor(props){
     super(props);
     this.state={
-      tasks: Array(3).fill('Tasks'),
+      taskList: Array(3).fill('Tasks'),
     };
+  }
+  clickHandler=()=>{
+    let newTask = document.getElementById('newTask').value;
+    let copyTaskList = this.state.taskList.slice();
+    copyTaskList.push(newTask);
+    this.setState({taskList:copyTaskList});
   }
   render(){
     return(
-      <div>
-        {this.state.tasks.map((item)=>{return <Task task={item} />})}
+      <div className="app">
+        <h1>Welcome</h1>
+        <form>
+          <input type="text"
+          name="taskName"
+          placeholder="Enter new task here"
+          id="newTask"></input>
+        </form>
+        <button onClick={this.clickHandler}>Add</button>
+        <br />
+        {this.state.taskList.map((item)=>{return <Task task={item} />})}
       </div>
     );
   }
