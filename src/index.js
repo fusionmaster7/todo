6412a1b1a7d0c44362d1ec985f946959954893cb
Hardik Singh <hardiksingh297@gmail.com>
2019-12-26 00:59:41 +0530

    Removed the App component for now. Rewrote the Task component to render directly using props instead of state.

--- src/index.js
+++ src/index.js
@@ -2,63 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-//Component to render Tasks.
+//Component to render each task
 class Task extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      task: null,
-    }
-  }
-  static getDerivedStateFromProps(props,state){
-    return {task:props.taskValue};
-  }
   render(){
     return(
       <div className="task-item">
-        <div className="task">
-          {this.state.task}
-        </div>
-        <button>Cancel</button>
+        <div className="task">{this.props.task}</div>
+        <button className="cancel">Cancel</button>
       </div>
     );
   }
 }
 
-//Component to render a List composed of tasks.
+//Component to render a list made up of tasks
 class List extends React.Component{
-    constructor(props){
-      super(props);
-      this.state = {
-        tasks: [],
-      };
-    }
-    renderTask = (data)=>{
-      return <Task taskValue = {data} />;
-    }
-    render(){
-      return(
-        <div>
-          {this.renderTask('Task 1')}
-          {this.state.tasks.map((item)=>this.renderTask(item))}
-        </div>
-      );
-    }
-}
-
-//Component to render the actual App.
-class App extends React.Component{
   render(){
     return(
-      <div className="app">
-        <h1>Welcome!</h1>
-        <form>
-          <input type="text" name="taskName" placeholder="Enter your Task"></input>
-          <button>Add</button>
-        </form>
-        <br />
-        <List />
+      <div>
+        <Task task="Task 1" />
+        <Task task="Task 2" />
+        <Task task="Task 3" />
       </div>
     );
   }
 }
+
+ReactDOM.render(<List />,document.getElementById('root'));
