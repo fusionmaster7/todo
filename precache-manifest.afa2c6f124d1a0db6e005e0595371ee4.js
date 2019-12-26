d7e1b08801292116ccfc9f7c8e131d40b459d65a
Hardik Singh <hardiksingh297@gmail.com>
2019-12-26 20:46:19 +0530

    Updates

---
+++ precache-manifest.afa2c6f124d1a0db6e005e0595371ee4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d19d7548e0ee2533a6a62c4a2ca4a71",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "a380a5186d5802fa4f69",
+    "url": "/todo/static/css/main.f7cefd30.chunk.css"
+  },
+  {
+    "revision": "7744fab8e07decaed1f8",
+    "url": "/todo/static/js/2.3481c240.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo/static/js/2.3481c240.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a380a5186d5802fa4f69",
+    "url": "/todo/static/js/main.238fd62d.chunk.js"
+  },
+  {
+    "revision": "9e3d90c4d53deff53c8a",
+    "url": "/todo/static/js/runtime-main.48ad9b76.js"
+  }
+]);
